1ae17cd1395e85a91b62f5ffd237123b1c2b33f2
76wenshu <1210238599@qq.com>
2020-09-28 21:26:12 +0800

    [0]DOM结构 [1]76wenshu [2]南京

--- JavascriptDOM/imageStorage4/js/imageFcunction.js
+++ JavascriptDOM/imageStorage4/js/imageFcunction.js
@@ -1,7 +1,10 @@
 function imageFunction(node){
+    //在点击事件中进行传值this那么就是相当于把这个点击是事件现有的DOM事件传出去
+    console.log(node)
     var source = node.getAttribute("href")
     var imageIdNode = document.getElementById('imageId');
     imageIdNode.setAttribute("src",source);
+    //将每一个的title属性获取出来会后进行在赋值
     var text = node.getAttribute('title');
     var description = document.getElementById('description');
     description.firstChild.nodeValue = text
